83f6adaba8f03435176fe4f4c1449dfc4cc976e4
ZIM <zim555@mail.ru>
2015-11-12 20:03:24 +0200

    Update settings

--- device_logands.mk
+++ device_logands.mk
@@ -40,8 +40,8 @@ PRODUCT_COPY_FILES += \
 #   device/samsung/baffinlite/MultiSIM-Toggle.apk:system/app/MultiSIM-Toggle.apk
 
 # Charger
-#PRODUCT_PACKAGES += \
-#	charger_res_images
+PRODUCT_PACKAGES += \
+	charger_res_images
 
 # Insecure ADBD
 ADDITIONAL_DEFAULT_PROPERTIES += \
@@ -117,17 +117,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	  
 # enable Google-specific location features,
 # like NetworkLocationProvider and LocationCollector
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    ro.com.google.locationfeatures=1 \
-#    ro.com.google.networklocation=1
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1
 
 # Extended JNI checks
 # The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
 # before they have a chance to cause problems.
 # Default=true for development builds, set by android buildsystem.
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    ro.kernel.android.checkjni=0 \
-#    dalvik.vm.checkjni=false
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0 \
+    dalvik.vm.checkjni=false
 
 # MTP
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
