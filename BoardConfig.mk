2f3685c48530321d100d7c4a5f86366038989073
ZIM <zim555@mail.ru>
2015-12-30 23:50:11 +0200

    Update test builds

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,26 +144,3 @@ TARGET_USES_LOGD := false
 
 # jemalloc causes a lot of random crash on free()
 MALLOC_IMPL := dlmalloc
-
-#BOARD_SEPOLICY_DIRS += \
-#    device/samsung/logands/sepolicy
-
-#BOARD_SEPOLICY_UNION += \
-#    file_contexts \
-#    property_contexts \
-#    service_contexts \
-#    bkmgrd.te \
-#    device.te \
-#	 surfaceflinger.te \
-#	 bluetooth.te \
-#    geomagneticd.te \
-#    gpsd.te \
-#    init.te \
-#    immvibed.te \
-#    kernel.te \
-#    macloader.te \
-#    rild.te \
-#    shell.te \
-#    system_server.te \
-#    tvserver.te \
-#    vclmk.te
